e5aefefdfcd5af8c567eff4efacacf140a4f376b
serpilcc <serpil60@hotmail.fr>
2017-11-03 17:16:15 +0100

    first commit

---
+++ db/migrate/20171103154338_create_events.rb
@@ -0,0 +1,11 @@
+class CreateEvents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :events do |t|
+      t.text :description
+      t.datetime :date
+      t.string :space
+
+      t.timestamps
+    end
+  end
+end
