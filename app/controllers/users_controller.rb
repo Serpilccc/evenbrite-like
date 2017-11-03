2dea629b4797173ff85d49d00d861f5ea980d236
serpilcc <serpil60@hotmail.fr>
2017-11-03 18:24:54 +0100

    second commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,26 @@
 class UsersController < ApplicationController
-def index
-end
-
-  def show
-    @user = User.find(params[:id])
+  def index
+  end
+  def new
+        @user=User.new
   end
 
-  def new
-  	@user=User.new
+  def show
+        @user = User.find(params[:id])
   end
 
   def create
-  	@user=User.new(user_params)
-  	if @user.save
+    session[:current_user_id] = @user.id
+    @user = User.new(user_params)
+    if @user.save
       log_in @user
       redirect_to @user
-  	else
-  		render 'new'
-  	end
+    else
+        render 'new'
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
 end
-
-private
-
-def user_params
-	params.require(:user).permit(:name, :email, :password, :password_confirmation)
-end
-
 end
